866f05d7a6824628d8ffb702ee5d7fb4bdad97fb
Eugene Lum <eugenelumtf@gmail.com>
2022-10-19 11:07:40 +0800

    changed navlink-usenavigate

--- client/src/components/Biddinglist/Biddinglist.jsx
+++ client/src/components/Biddinglist/Biddinglist.jsx
@@ -5,182 +5,139 @@ import Button from "react-bootstrap/esm/Button";
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { NavLink } from "react-router-dom";
+import {useNavigate } from "react-router-dom";
 import Card from "../Card/Card.jsx";
 
 const BiddingList = (props) => {
-    const dummyBidGroup = [
-        [
-            {
-                name: "JonLee.ens",
-                start: "20/10/22 18:30",
-                end: "20/10/22 19:30",
-                id: 1,
-            },
-            {
-                name: "GeneLum.ens",
-                start: "19/10/22 16:30",
-                end: "19/10/22 17:30",
-                id: 2,
-            },
-            {
-                name: "Chuansong.ens",
-                start: "10/10/22 01:00",
-                end: "10/10/22 02:00",
-                id: 3,
-            },
-        ],
-        [
-            {
-                name: "CatChew.ens",
-                start: "09/09/22 18:30",
-                end: "09/09/22 19:30",
-                id: 4,
-            },
-            {
-                name: "JonLee.ens",
-                start: "09/09/22 18:30",
-                end: "09/09/22 19:30",
-                id: 5,
-            },
-            {
-                name: "GeneLum.ens",
-                start: "09/09/22 18:30",
-                end: "09/09/22 19:30",
-                id: 6,
-            },
-        ],
-        [
-            {
-                name: "CatChew.ens",
-                start: "09/09/22 18:30",
-                end: "09/09/22 19:30",
-                id: 8,
-            },
-            {
-                name: "Chuansong.ens",
-                start: "09/09/22 18:30",
-                end: "09/09/22 19:30",
-                id: 7,
-            },
-        ],
-    ];
+  const dummyBidGroup = [
+    [
+      {
+        name: "JonLee.ens",
+        start: "20/10/22 18:30",
+        end: "20/10/22 19:30",
+        id: 1,
+      },
+      {
+        name: "GeneLum.ens",
+        start: "19/10/22 16:30",
+        end: "19/10/22 17:30",
+        id: 2,
+      },
+      {
+        name: "Chuansong.ens",
+        start: "10/10/22 01:00",
+        end: "10/10/22 02:00",
+        id: 3,
+      },
+    ],
+    [
+      {
+        name: "CatChew.ens",
+        start: "09/09/22 18:30",
+        end: "09/09/22 19:30",
+        id: 4,
+      },
+      {
+        name: "JonLee.ens",
+        start: "09/09/22 18:30",
+        end: "09/09/22 19:30",
+        id: 5,
+      },
+      {
+        name: "GeneLum.ens",
+        start: "09/09/22 18:30",
+        end: "09/09/22 19:30",
+        id: 6,
+      },
+    ],
+    [
+      {
+        name: "CatChew.ens",
+        start: "09/09/22 18:30",
+        end: "09/09/22 19:30",
+        id: 8,
+      },
+      {
+        name: "Chuansong.ens",
+        start: "09/09/22 18:30",
+        end: "09/09/22 19:30",
+        id: 7,
+      },
+    ],
+  ];
 
-    const [auctions, setAuctions] = useState();
-    const [loading, setLoading] = useState(false);
+  const [auctions, setAuctions] = useState();
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-    const {
-        connectWallet,
-        checkIfWalletIsConnected,
-        connected,
-        createAuction,
-        getAuctions,
-    } = useContext(DnsContext);
 
-    useEffect(() => {
-        const getAuctionsHandler = async () => {
-            const data = await getAuctions();
+  const {
+    connectWallet,
+    checkIfWalletIsConnected,
+    connected,
+    createAuction,
+    getAuctions,
+  } = useContext(DnsContext);
 
-            const auctionsMapped = await Promise.all(
-                data.map(async (i) => {
-                    const unixTime = i.biddingEnd.toNumber();
-                    let date = new Date(unixTime * 1000);
-                    date = date.toUTCString();
+  useEffect(() => {
+    const getAuctionsHandler = async () => {
+      const data = await getAuctions();
 
-                    let auctionItem = {
-                        auctionId: i.auctionId.toNumber(),
-                        name: i.name,
-                        biddingEnd: date,
-                        // revealEnd: i.revealEnd,
-                    };
-                    return auctionItem;
-                })
+      const auctionsMapped = await Promise.all(
+        data.map(async (i) => {
+          const unixTime = i.biddingEnd.toNumber();
+          let date = new Date(unixTime * 1000);
+          date = date.toUTCString();
+
+          let auctionItem = {
+            auctionId: i.auctionId.toNumber(),
+            name: i.name,
+            biddingEnd: date,
+            // revealEnd: i.revealEnd,
+          };
+          return auctionItem;
+        })
+      );
+      setLoading(true);
+      setAuctions(auctionsMapped);
+      setLoading(false);
+    };
+
+    getAuctionsHandler();
+
+    console.log(auctions);
+  }, [getAuctions]);
+
+  let bidPath = "";
+  if (props.connected) {
+    bidPath = "/placebid";
+  } else {
+    bidPath = "/connect";
+  }
+  return (
+    <div>
+      {loading && !auctions && <p>Loading</p>}
+
+      {!loading && auctions && (
+        <Container>
+          <h2 className={styles.pagename}>Bidding List</h2>
+          {auctions.map((auction) => {
+            return (
+              <Col lg={4} md={6} sm={12} xs={12}>
+                <Card className={styles.card} key={auction.id}>
+                  <p>Domain Name: {auction.name}</p>
+                  <p>End: {auction.biddingEnd}</p>
+                  <Button className={`btn btn-primary ${styles.bidbtn}`} onClick={()=>{navigate(bidPath,{state:{name:auction.name}})}}>
+                    Place a Bid!
+                  </Button>
+                </Card>
+              </Col>
             );
-            setLoading(true);
-            setAuctions(auctionsMapped);
-            setLoading(false);
-        };
-
-        getAuctionsHandler();
-
-        console.log(auctions);
-    }, [getAuctions]);
-
-    let bidPath = "";
-    if (props.connected) {
-        bidPath = "/placebid";
-    } else {
-        bidPath = "/connect";
-    }
-    return (
-        <div>
-            {loading && !auctions && <p>Loading</p>}
-
-            {!loading && auctions && (
-                <Container>
-                    <h2 className={styles.pagename}>Bidding List</h2>
-                    {auctions.map((auction) => {
-                        return (
-                            <Col lg={4} md={6} sm={12} xs={12}>
-                                <Card className={styles.card} key={auction.id}>
-                                    <p>Domain Name: {auction.name}</p>
-                                    <p>End: {auction.biddingEnd}</p>
-                                    <Button
-                                        className={`btn btn-primary ${styles.bidbtn}`}
-                                    >
-                                        <NavLink
-                                            to={bidPath}
-                                            state={{
-                                                name: `${auction.name}`,
-                                            }}
-                                            className={styles.bidlink}
-                                        >
-                                            Place a Bid!
-                                        </NavLink>
-                                    </Button>
-                                </Card>
-                            </Col>
-                        );
-                    })}
-                    {/* {dummyBidGroup.map((dummyBid) => {
-                        return (
-                            <Row>
-                                {dummyBid.map((bid) => {
-                                    return (
-                                        <Col lg={4} md={6} sm={12} xs={12}>
-                                            <Card
-                                                className={styles.card}
-                                                key={bid.id}
-                                            >
-                                                <p>Domain Name: {bid.name}</p>
-                                                <p>Start: {bid.start}</p>
-                                                <p>End: {bid.end}</p>
-                                                <Button
-                                                    className={`btn btn-primary ${styles.bidbtn}`}
-                                                >
-                                                    <NavLink
-                                                        to={bidPath}
-                                                        state={{
-                                                            name: `${bid.name}`,
-                                                        }}
-                                                        className={
-                                                            styles.bidlink
-                                                        }
-                                                    >
-                                                        Place a Bid!
-                                                    </NavLink>
-                                                </Button>
-                                            </Card>
-                                        </Col>
-                                    );
-                                })}
-                            </Row>
-                        );
-                    })} */}
-                </Container>
-            )}
-        </div>
-    );
+          })}
+        </Container>
+      )}
+    </div>
+  );
 };
 
 export default BiddingList;
--- client/src/components/Placebid/Placebid.jsx
+++ client/src/components/Placebid/Placebid.jsx
@@ -3,166 +3,161 @@ import { useState, useEffect, useContext } from "react";
 import { ethers } from "ethers";
 import { DnsContext } from "../../context/DnsContext";
 import Button from "react-bootstrap/Button";
-import { NavLink, useLocation } from "react-router-dom";
+import {useLocation,useNavigate } from "react-router-dom";
 import { Icon1Circle, Icon2Circle, Icon3Circle } from "react-bootstrap-icons";
 
 const Placebid = (props) => {
-    const [rentalPrice, setRentalPrice] = useState("");
-    const [domainName, setDomainName] = useState("");
-    const [secretInt, setSecretInt] = useState("");
+  const [rentalPrice, setRentalPrice] = useState("");
+  const [domainName, setDomainName] = useState("");
+  const [secretInt, setSecretInt] = useState("");
 
-    const location = useLocation();
-    const { state } = location;
-    let existingBid = false;
+  const location = useLocation();
+  const { state } = location;
+  let existingBid = false;
+  const navigate = useNavigate();
+  console.log("this is state",state);
+  const cancelHandler = () => {
+    navigate("/");
+  }
 
-    const {
-        // connectWallet,
-        // checkIfWalletIsConnected,
-        // connected,
-        loading,
-        createAuctionAndBid,
-    } = useContext(DnsContext);
+  const {
+    // connectWallet,
+    // checkIfWalletIsConnected,
+    // connected,
+    loading,
+    createAuctionAndBid,
+  } = useContext(DnsContext);
 
-    if (!state) {
-        existingBid = false;
-    } else {
-        existingBid = true;
-    }
+  if (!state) {
+    existingBid = false;
+  } else {
+    existingBid = true;
+  }
 
-    const domainNameHandler = (event) => {
-        setDomainName(event.target.value);
-    };
+  const domainNameHandler = (event) => {
+    setDomainName(event.target.value);
+  };
 
-    const priceHandler = (event) => {
-        setRentalPrice(event.target.value);
-    };
+  const priceHandler = (event) => {
+    setRentalPrice(event.target.value);
+  };
 
-    const secretHandler = (event) => {
-        setSecretInt(event.target.value);
-    };
+  const secretHandler = (event) => {
+    setSecretInt(event.target.value);
+  };
 
-    const submitHandler = (event) => {
-        event.preventDefault();
-        console.log("submitted!");
-    };
-    return (
-        <form className={`${styles.mainpage}`} onSubmit={submitHandler}>
-            <div className={`${styles.topbar}`}>
-                {existingBid && (
-                    <p>
-                        Domain Name:{" "}
-                        <span className={styles.domainname}>
-                            <em>{state.name}</em>
-                        </span>
-                    </p>
-                )}
-                {!existingBid && (
-                    <div className={styles.newauction}>
-                        <label htmlFor="nameinput">Domain Name: </label>
-                        <input
-                            id="nameinput"
-                            type="text"
-                            placeholder="Enter Domain Name"
-                            onChange={domainNameHandler}
-                            value={domainName}
-                            className={styles.nameinput}
-                        />
-                        <p className={styles.domain}>.ntu</p>
-                    </div>
-                )}
-                <div className={`${styles.price}`}>
-                    <label htmlFor="rentalprice">Bid: </label>
-                    <input
-                        id="rentalprice"
-                        type="text"
-                        placeholder="0"
-                        pattern="^\d*(\.\d{0,6})?$"
-                        value={rentalPrice}
-                        onChange={priceHandler}
-                        className={styles.rentalprice}
-                    />
-                </div>
-                <div className={`${styles.secret}`}>
-                    <label htmlFor="secret">Secret: </label>
-                    <input
-                        id="secret"
-                        type="password"
-                        pattern="\d*"
-                        value={secretInt}
-                        onChange={secretHandler}
-                        className={styles.secretInt}
-                    />
-                </div>
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log("submitted!");
+  };
+  return (
+    <form className={`${styles.mainpage}`} onSubmit={submitHandler}>
+      <div className={`${styles.topbar}`}>
+        {existingBid && (
+          <p>
+            Domain Name:{" "}
+            <span className={styles.domainname}>
+              <em>{state.name}.ntu</em>
+            </span>
+          </p>
+        )}
+        {!existingBid && (
+          <div className={styles.newauction}>
+            <label htmlFor="nameinput">Domain Name: </label>
+            <input
+              id="nameinput"
+              type="text"
+              placeholder="Enter Domain Name"
+              onChange={domainNameHandler}
+              value={domainName}
+              className={styles.nameinput}
+            />
+            <p className={styles.domain}>.ntu</p>
+          </div>
+        )}
+        <div className={`${styles.price}`}>
+          <label htmlFor="rentalprice">Bid: </label>
+          <input
+            id="rentalprice"
+            type="text"
+            placeholder="0"
+            pattern="^\d*(\.\d{0,6})?$"
+            value={rentalPrice}
+            onChange={priceHandler}
+            className={styles.rentalprice}
+          />
+        </div>
+        <div className={`${styles.secret}`}>
+          <label htmlFor="secret">Secret: </label>
+          <input
+            id="secret"
+            type="password"
+            pattern="\d*"
+            value={secretInt}
+            onChange={secretHandler}
+            className={styles.secretInt}
+          />
+        </div>
+      </div>
+      <div className={styles.middle}>
+        <h2>
+          Registering a domain name requires you to complete these steps:{" "}
+        </h2>
+        {existingBid && (
+          <div className={styles.steps}>
+            <div className={styles.step}>
+              <Icon1Circle size={50} />
+              <span>
+                <h4>New Auction</h4>
+                <p>Enter domain name to check for availability </p>
+              </span>
             </div>
-            <div className={styles.middle}>
-                <h2>
-                    Registering a domain name requires you to complete these
-                    steps:{" "}
-                </h2>
-                <div className={styles.steps}>
-                    <div className={styles.step}>
-                        <Icon1Circle size={50} />
-                        <span>
-                            <h4>New Auction</h4>
-                            <p>Enter domain name to check for availability </p>
-                        </span>
-                    </div>
-                    <div className={styles.step}>
-                        <Icon2Circle size={50} />
-                        <span>
-                            <h4>Bidding Amount</h4>
-                            <p>
-                                Enter bid for blind auction.
-                                <br />
-                            </p>
-                            <span className={styles.note}>
-                                <ol>
-                                    Note:<li>Bid cannot be withdrawn</li>
-                                    <li>
-                                        Bid amount cannot be changed once
-                                        committed
-                                    </li>
-                                </ol>
-                            </span>
-                        </span>
-                    </div>
-                    <div className={styles.step}>
-                        <Icon3Circle size={50} />
-                        <span>
-                            <h4>Register</h4>
-                            <p>Hit the register button to commit your bid! </p>
-                        </span>
-                    </div>
-                </div>
+            <div className={styles.step}>
+              <Icon2Circle size={50} />
+              <span>
+                <h4>Bidding Amount</h4>
+                <p>
+                  Enter bid for blind auction.
+                  <br />
+                </p>
+                <span className={styles.note}>
+                  <ol>
+                    Note:<li>Bid cannot be withdrawn</li>
+                    <li>Bid amount cannot be changed once committed</li>
+                  </ol>
+                </span>
+              </span>
             </div>
-            <div className={`${styles.bottombtn}`}>
-                <Button
-                    className={`btn btn-light btn-lg ${styles.reqregisterbtn}`}
-                >
-                    <NavLink to="/" className={styles.cancelbtn}>
-                        Cancel
-                    </NavLink>
-                </Button>
-                {loading ? (
-                    <p>Loading</p>
-                ) : (
-                    <Button
-                        type="submit"
-                        className={`btn btn-primary btn-lg ${styles.reqregisterbtn}`}
-                        onClick={() => {
-                            createAuctionAndBid(
-                                domainName,
-                                rentalPrice,
-                                secretInt
-                            );
-                        }}
-                    >
-                        Register!
-                    </Button>
-                )}
+            <div className={styles.step}>
+              <Icon3Circle size={50} />
+              <span>
+                <h4>Register</h4>
+                <p>Hit the register button to commit your bid! </p>
+              </span>
             </div>
-        </form>
-    );
+          </div>
+        )}
+      </div>
+      <div className={`${styles.bottombtn}`}>
+        <Button className={`btn btn-light btn-lg ${styles.reqregisterbtn}`} onClick={cancelHandler}>
+            Cancel
+        </Button>
+        {loading ? (
+          <p>Loading</p>
+        ) : (
+          <Button
+            type="submit"
+            className={`btn btn-primary btn-lg ${styles.reqregisterbtn}`}
+            onClick={() => {
+              createAuctionAndBid(domainName, rentalPrice, secretInt);
+            }}
+          >
+            Register!
+          </Button>
+        )}
+      </div>
+    </form>
+  );
 };
 
 export default Placebid;
