f21945d9ca56ce8b447ec764a6c7634e9b3275ec
Eugene Lum <eugenelumtf@gmail.com>
2022-10-21 16:18:38 +0800

    errorhandler

--- client/src/components/Biddinglist/Biddinglist.jsx
+++ client/src/components/Biddinglist/Biddinglist.jsx
@@ -7,156 +7,170 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { useNavigate } from "react-router-dom";
 import Card from "../Card/Card.jsx";
+import ErrorModal from "../ErrorModal/ErrorModal";
 
 const BiddingList = (props) => {
-    const dummyBidGroup = [
-        [
-            {
-                name: "JonLee.ens",
-                start: "20/10/22 18:30",
-                end: "20/10/22 19:30",
-                id: 1,
-            },
-            {
-                name: "GeneLum.ens",
-                start: "19/10/22 16:30",
-                end: "19/10/22 17:30",
-                id: 2,
-            },
-            {
-                name: "Chuansong.ens",
-                start: "10/10/22 01:00",
-                end: "10/10/22 02:00",
-                id: 3,
-            },
-        ],
-        [
-            {
-                name: "CatChew.ens",
-                start: "09/09/22 18:30",
-                end: "09/09/22 19:30",
-                id: 4,
-            },
-            {
-                name: "JonLee.ens",
-                start: "09/09/22 18:30",
-                end: "09/09/22 19:30",
-                id: 5,
-            },
-            {
-                name: "GeneLum.ens",
-                start: "09/09/22 18:30",
-                end: "09/09/22 19:30",
-                id: 6,
-            },
-        ],
-        [
-            {
-                name: "CatChew.ens",
-                start: "09/09/22 18:30",
-                end: "09/09/22 19:30",
-                id: 8,
-            },
-            {
-                name: "Chuansong.ens",
-                start: "09/09/22 18:30",
-                end: "09/09/22 19:30",
-                id: 7,
-            },
-        ],
-    ];
+  const dummyBidGroup = [
+    [
+      {
+        name: "JonLee.ens",
+        start: "20/10/22 18:30",
+        end: "20/10/22 19:30",
+        id: 1,
+      },
+      {
+        name: "GeneLum.ens",
+        start: "19/10/22 16:30",
+        end: "19/10/22 17:30",
+        id: 2,
+      },
+      {
+        name: "Chuansong.ens",
+        start: "10/10/22 01:00",
+        end: "10/10/22 02:00",
+        id: 3,
+      },
+    ],
+    [
+      {
+        name: "CatChew.ens",
+        start: "09/09/22 18:30",
+        end: "09/09/22 19:30",
+        id: 4,
+      },
+      {
+        name: "JonLee.ens",
+        start: "09/09/22 18:30",
+        end: "09/09/22 19:30",
+        id: 5,
+      },
+      {
+        name: "GeneLum.ens",
+        start: "09/09/22 18:30",
+        end: "09/09/22 19:30",
+        id: 6,
+      },
+    ],
+    [
+      {
+        name: "CatChew.ens",
+        start: "09/09/22 18:30",
+        end: "09/09/22 19:30",
+        id: 8,
+      },
+      {
+        name: "Chuansong.ens",
+        start: "09/09/22 18:30",
+        end: "09/09/22 19:30",
+        id: 7,
+      },
+    ],
+  ];
 
-    const [auctions, setAuctions] = useState();
-    const [loading, setLoading] = useState(false);
-    const navigate = useNavigate();
+  const [auctions, setAuctions] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+  let errorMsg = "";
+  let bidPath = "";
 
-    const {
-        // connectWallet,
-        // checkIfWalletIsConnected,
-        // connected,
-        // createAuction,
-        bid,
-        getAuctions,
-    } = useContext(DnsContext);
+  if (props.connected) {
+    bidPath = "/placebid";
+  } else {
+    bidPath = "/connect";
+  }
 
-    useEffect(() => {
-        const getAuctionsHandler = async () => {
-            const data = await getAuctions();
+  const { bid, getAuctions } = useContext(DnsContext);
 
-            const auctionsMapped = await Promise.all(
-                data.map(async (i) => {
-                    const unixStart = i.start.toNumber();
-                    let startDate = new Date(unixStart * 1000);
-                    startDate = startDate.toUTCString();
+  useEffect(() => {
+    const getAuctionsHandler = async () => {
+      try {
+        const data = await getAuctions();
 
-                    const unixEnd = i.revealEnd.toNumber();
-                    let endDate = new Date(unixEnd * 1000);
-                    endDate = endDate.toUTCString();
+        const auctionsMapped = await Promise.all(
+          data.map(async (i) => {
+            const unixStart = i.start.toNumber();
+            let startDate = new Date(unixStart * 1000);
+            startDate = startDate.toUTCString();
 
-                    const unixTime = i.biddingEnd.toNumber();
-                    let date = new Date(unixTime * 1000);
-                    date = date.toUTCString();
+            const unixEnd = i.revealEnd.toNumber();
+            let endDate = new Date(unixEnd * 1000);
+            endDate = endDate.toUTCString();
 
-                    let auctionItem = {
-                        auctionId: i.auctionId.toNumber(),
-                        name: i.name,
-                        start: startDate,
-                        biddingEnd: date,
-                        revealEnd: endDate,
-                    };
-                    return auctionItem;
-                })
+            const unixTime = i.biddingEnd.toNumber();
+            let date = new Date(unixTime * 1000);
+            date = date.toUTCString();
+
+            let auctionItem = {
+              auctionId: i.auctionId.toNumber(),
+              name: i.name,
+              start: startDate,
+              biddingEnd: date,
+              revealEnd: endDate,
+            };
+            return auctionItem;
+          })
+        );
+        setLoading(true);
+        setAuctions(auctionsMapped);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+        setError(true);
+        errorMsg = err.msg;
+      }
+    };
+
+    getAuctionsHandler();
+
+    console.log(auctions);
+  }, [getAuctions]);
+
+  const errorHandler = (event) => {
+    setError(!error);
+  };
+
+  return (
+    <div>
+      {error && (
+        <ErrorModal
+          title="Error occurred"
+          message={errorMsg}
+          onConfirm={errorHandler}
+        />
+      )}
+      {loading && !auctions && <p>Loading</p>}
+
+      {!loading && auctions && (
+        <Container>
+          <h2 className={styles.pagename}>Bidding List</h2>
+          {auctions.map((auction) => {
+            return (
+              <Col lg={4} md={6} sm={12} xs={12}>
+                <Card className={styles.card} key={auction.id}>
+                  <p>Domain Name: {auction.name}</p>
+                  <p>Start: {auction.start}</p>
+                  <p>Bidding End: {auction.biddingEnd}</p>
+                  <p>Reveal End: {auction.revealEnd}</p>
+
+                  <Button
+                    className={`btn btn-primary ${styles.bidbtn}`}
+                    onClick={() => {
+                      navigate(bidPath, {
+                        state: { name: auction.name },
+                      });
+                    }}
+                  >
+                    Place a Bid!
+                  </Button>
+                </Card>
+              </Col>
             );
-            setLoading(true);
-            setAuctions(auctionsMapped);
-            setLoading(false);
-        };
-
-        getAuctionsHandler();
-
-        console.log(auctions);
-    }, [getAuctions]);
-
-    let bidPath = "";
-    if (props.connected) {
-        bidPath = "/placebid";
-    } else {
-        bidPath = "/connect";
-    }
-    return (
-        <div>
-            {loading && !auctions && <p>Loading</p>}
-
-            {!loading && auctions && (
-                <Container>
-                    <h2 className={styles.pagename}>Bidding List</h2>
-                    {auctions.map((auction) => {
-                        return (
-                            <Col lg={4} md={6} sm={12} xs={12}>
-                                <Card className={styles.card} key={auction.id}>
-                                    <p>Domain Name: {auction.name}</p>
-                                    <p>Start: {auction.start}</p>
-                                    <p>Bidding End: {auction.biddingEnd}</p>
-                                    <p>Reveal End: {auction.revealEnd}</p>
-
-                                    <Button
-                                        className={`btn btn-primary ${styles.bidbtn}`}
-                                        onClick={() => {
-                                            navigate(bidPath, {
-                                                state: { name: auction.name },
-                                            });
-                                        }}
-                                    >
-                                        Place a Bid!
-                                    </Button>
-                                </Card>
-                            </Col>
-                        );
-                    })}
-                </Container>
-            )}
-        </div>
-    );
+          })}
+        </Container>
+      )}
+    </div>
+  );
 };
 
 export default BiddingList;
--- client/src/components/Mybiddings/Mybiddings.jsx
+++ client/src/components/Mybiddings/Mybiddings.jsx
@@ -7,182 +7,190 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "../Card/Card.jsx";
 import InputModal from "../InputModal/InputModal";
+import ErrorModal from "../ErrorModal/ErrorModal";
 // import { useEffect } from "react";
 
 const Mybiddings = (props) => {
-    const dummyBidGroup = [
-        [
-            {
-                name: "JonLee.ens",
-                start: "20/10/22 18:30",
-                end: "20/10/22 19:30",
-                id: 1,
-            },
-            {
-                name: "GeneLum.ens",
-                start: "19/10/22 16:30",
-                end: "19/10/22 17:30",
-                id: 2,
-            },
-            {
-                name: "Chuansong.ens",
-                start: "10/10/22 01:00",
-                end: "10/10/22 02:00",
-                id: 3,
-            },
-        ],
-        [
-            {
-                name: "CatChew.ens",
-                start: "09/09/22 18:30",
-                end: "09/09/22 19:30",
-                id: 4,
-            },
-            {
-                name: "JonLee.ens",
-                start: "09/09/22 18:30",
-                end: "09/09/22 19:30",
-                id: 5,
-            },
-            {
-                name: "GeneLum.ens",
-                start: "09/09/22 18:30",
-                end: "09/09/22 19:30",
-                id: 6,
-            },
-        ],
-        [
-            {
-                name: "CatChew.ens",
-                start: "09/09/22 18:30",
-                end: "09/09/22 19:30",
-                id: 8,
-            },
-            {
-                name: "Chuansong.ens",
-                start: "09/09/22 18:30",
-                end: "09/09/22 19:30",
-                id: 7,
-            },
-        ],
-    ];
+  const dummyBidGroup = [
+    [
+      {
+        name: "JonLee.ens",
+        start: "20/10/22 18:30",
+        end: "20/10/22 19:30",
+        id: 1,
+      },
+      {
+        name: "GeneLum.ens",
+        start: "19/10/22 16:30",
+        end: "19/10/22 17:30",
+        id: 2,
+      },
+      {
+        name: "Chuansong.ens",
+        start: "10/10/22 01:00",
+        end: "10/10/22 02:00",
+        id: 3,
+      },
+    ],
+    [
+      {
+        name: "CatChew.ens",
+        start: "09/09/22 18:30",
+        end: "09/09/22 19:30",
+        id: 4,
+      },
+      {
+        name: "JonLee.ens",
+        start: "09/09/22 18:30",
+        end: "09/09/22 19:30",
+        id: 5,
+      },
+      {
+        name: "GeneLum.ens",
+        start: "09/09/22 18:30",
+        end: "09/09/22 19:30",
+        id: 6,
+      },
+    ],
+    [
+      {
+        name: "CatChew.ens",
+        start: "09/09/22 18:30",
+        end: "09/09/22 19:30",
+        id: 8,
+      },
+      {
+        name: "Chuansong.ens",
+        start: "09/09/22 18:30",
+        end: "09/09/22 19:30",
+        id: 7,
+      },
+    ],
+  ];
 
-    const {
-        // connectWallet,
-        // checkIfWalletIsConnected,
-        // connected,
-        // createAuction,
-        // loading,
-        revealBid,
-        getMyBiddings,
-    } = useContext(DnsContext);
-    const [loading, setLoading] = useState(false);
-    const [bids, setBiddings] = useState();
+  const {
+    // connectWallet,
+    // checkIfWalletIsConnected,
+    // connected,
+    // createAuction,
+    // loading,
+    revealBid,
+    getMyBiddings,
+  } = useContext(DnsContext);
+  const [loading, setLoading] = useState(false);
+  const [bids, setBiddings] = useState();
 
-    useEffect(() => {
-        const getBids = async () => {
-            const data = await getMyBiddings(props.connected);
-            const biddingsMapped = await Promise.all(
-                data.map(async (i) => {
-                    const unixStart = i.start.toNumber();
-                    let startDate = new Date(unixStart * 1000);
-                    startDate = startDate.toUTCString();
+  let errorMsg = ""
+  useEffect(() => {
+    const getBids = async () => {
+      try {
+        const data = await getMyBiddings(props.connected);
+        const biddingsMapped = await Promise.all(
+          data.map(async (i) => {
+            const unixStart = i.start.toNumber();
+            let startDate = new Date(unixStart * 1000);
+            startDate = startDate.toUTCString();
 
-                    const unixEnd = i.end.toNumber();
-                    let endDate = new Date(unixEnd * 1000);
-                    endDate = endDate.toUTCString();
-                    let bidItem = {
-                        name: i.name,
-                        start: startDate,
-                        end: endDate,
-                        revealed: i.revealed,
-                    };
-                    return bidItem;
-                })
-            );
+            const unixEnd = i.end.toNumber();
+            let endDate = new Date(unixEnd * 1000);
+            endDate = endDate.toUTCString();
+            let bidItem = {
+              name: i.name,
+              start: startDate,
+              end: endDate,
+              revealed: i.revealed,
+            };
+            return bidItem;
+          })
+        );
 
-            setBiddings(biddingsMapped);
+        setBiddings(biddingsMapped);
 
-            console.log(bids);
-        };
-        setLoading(true);
-        getBids();
-        setLoading(false);
-    }, [getMyBiddings]);
-
-    let bidPath = "";
-    if (props.connected) {
-        bidPath = "/placebid";
-    } else {
-        bidPath = "/connect";
-    }
-
-    const [reveal, setReveal] = useState(false);
-    const [secret, setSecret] = useState();
-    const [name, setName] = useState("");
-
-    const revealBidSubmit = async () => {
-        console.log("secret ", secret, " name ", name);
-
-        await revealBid(name, secret);
-        revealHandler();
+        console.log(bids);
+      } catch (err) {
+        setError(true);
+        setReveal(false);
+        errorMsg = err.msg;
+      }
     };
+    setLoading(true);
+    getBids();
+    setLoading(false);
+  }, [getMyBiddings]);
 
-    const revealHandler = (event) => {
-        setReveal(!reveal);
-    };
+  const [reveal, setReveal] = useState(false);
+  const [secret, setSecret] = useState();
+  const [name, setName] = useState("");
+  const [error,setError] = useState(false);
 
-    const secretHandler = (event) => {
-        setSecret(event.target.value);
-    };
+  const revealBidSubmit = async () => {
+    console.log("secret ", secret, " name ", name);
 
-    return (
-        <Container>
-            {reveal && (
-                <InputModal
-                    onConfirm={() => {
-                        revealBidSubmit();
-                    }}
-                    title="Reveal"
-                    placeholder="Please input secret integer"
-                    type="password"
-                    pattern="\d*"
-                    label="Secret:"
-                    onChange={secretHandler}
-                    value={secret}
-                />
-            )}
-            <h2 className={styles.pagename}>My Biddings</h2>
-            <Row>
-                {loading && !bids && <p>Loading</p>}
-                {!loading && bids && (
-                    <div>
-                        {bids.map((bid) => {
-                            return (
-                                <Col lg={4} md={6} sm={12} xs={12}>
-                                    <Card className={styles.card} key={bid.id}>
-                                        <p>Domain Name: {bid.name}</p>
-                                        <p>Start: {bid.start}</p>
-                                        <p>End: {bid.end}</p>
-                                        <p>Revealed: {bid.revealed.toString()}</p>
-                                        <Button
-                                            className={`btn btn-primary ${styles.bidbtn}`}
-                                            onClick={() => {
-                                                setName(bid.name);
-                                                revealHandler();
-                                            }}
-                                        >
-                                            Check status
-                                        </Button>
-                                    </Card>
-                                </Col>
-                            );
-                        })}
-                    </div>
-                )}
-            </Row>
-        </Container>
-    );
+    await revealBid(name, secret);
+    revealHandler();
+  };
+
+  const revealHandler = (event) => {
+    setReveal(!reveal);
+  };
+
+  const secretHandler = (event) => {
+    setSecret(event.target.value);
+  };
+
+  const errorHandler = (event) =>{
+    setError(!error)
+  }
+
+  return (
+    <Container>
+    {error && <ErrorModal title="Error occurred" message={errorMsg} onConfirm={errorHandler}/>}
+      {reveal && (
+        <InputModal
+          onConfirm={() => {
+            revealBidSubmit();
+          }}
+          title="Reveal"
+          placeholder="Please input secret integer"
+          type="password"
+          pattern="\d*"
+          label="Secret:"
+          onChange={secretHandler}
+          value={secret}
+        />
+      )}
+      <h2 className={styles.pagename}>My Biddings</h2>
+      <Row>
+      
+        {loading && !bids && <p>Loading</p>}
+        {!loading && bids && (
+          <div>
+            {bids.map((bid) => {
+              return (
+                <Col lg={4} md={6} sm={12} xs={12}>
+                  <Card className={styles.card} key={bid.id}>
+                    <p>Domain Name: {bid.name}</p>
+                    <p>Start: {bid.start}</p>
+                    <p>End: {bid.end}</p>
+                    <p>Revealed: {bid.revealed.toString()}</p>
+                    <Button
+                      className={`btn btn-primary ${styles.bidbtn}`}
+                      onClick={() => {
+                        setName(bid.name);
+                        revealHandler();
+                      }}
+                    >
+                      Check status
+                    </Button>
+                  </Card>
+                </Col>
+              );
+            })}
+          </div>
+        )}
+      </Row>
+    </Container>
+  );
 };
 
 export default Mybiddings;
