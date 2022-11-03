b993309db773727adfe6fb2ed13f40a60130628f
Jonathan <jonlee121198@gmail.com>
2022-11-03 21:39:15 +0800

    final final final

--- smartContracts/hardhat.config.js
+++ smartContracts/hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     goerli:{
       url: "https://eth-goerli.g.alchemy.com/v2/vinPz4TyViMcgfk8bnCHZEL7UEOpZsvm",
-      accounts:['fcde2f83061645a218fc5f883d4df43a7a7e40c1d4bdd22fe366e384797670e9']
+      accounts:['']
     }
   }
 };
